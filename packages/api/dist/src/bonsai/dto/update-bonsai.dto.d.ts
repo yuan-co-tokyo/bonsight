9c10379a36457cdf06e0e85c59c9695038db9c0e
yuan-co-tokyo <yuan.co.tokyo@gmail.com>
2026-06-22 13:04:19 +0900

    chore(api): stop tracking build output — git rm --cached api/dist (DIST-TRACKED-001)

--- packages/api/dist/src/bonsai/dto/update-bonsai.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateBonsaiDto } from './create-bonsai.dto';
-declare const UpdateBonsaiDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateBonsaiDto>>;
-export declare class UpdateBonsaiDto extends UpdateBonsaiDto_base {
-}
-export {};
--- packages/api/dist/src/bonsai/dto/update-bonsai.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateBonsaiDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const create_bonsai_dto_1 = require("./create-bonsai.dto");
-class UpdateBonsaiDto extends (0, mapped_types_1.PartialType)(create_bonsai_dto_1.CreateBonsaiDto) {
-}
-exports.UpdateBonsaiDto = UpdateBonsaiDto;
-//# sourceMappingURL=update-bonsai.dto.js.map
