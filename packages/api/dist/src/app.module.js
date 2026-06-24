5ce4a4e7e5ea0006ca20d1285a6b6fa5e09bf796
yuan-co-tokyo <yuan.co.tokyo@gmail.com>
2026-06-24 09:11:37 +0900

    AWS Settings

--- packages/api/dist/src/app.module.js
+++ packages/api/dist/src/app.module.js
@@ -8,16 +8,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
+const advice_module_1 = require("./advice/advice.module");
+const bonsai_module_1 = require("./bonsai/bonsai.module");
+const chat_module_1 = require("./chat/chat.module");
+const me_module_1 = require("./me/me.module");
+const media_module_1 = require("./media/media.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
-        controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
+        imports: [bonsai_module_1.BonsaiModule, media_module_1.MediaModule, advice_module_1.AdviceModule, chat_module_1.ChatModule, me_module_1.MeModule],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
